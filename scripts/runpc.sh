4c6681919366e3c3bc092ac0bad28d2b83e2fd7d
Daniel San Martin <dsanmartinreyes@gmail.com>
2025-05-05 18:59:31 -0400

    Testing bash script

--- scripts/runpc.sh
+++ scripts/runpc.sh
@@ -2,4 +2,4 @@
 # Run on background no hangup
 # nohup ./bin/wildfires data/input/parameters.txt > wildfire.out 2>&1 &
 # nohup ./bin/wildfires_cu data/input/f19_gaussian.txt &
-nohup ./bin/wildfires_cu data/input/debug.txt > variable.out 2>&1 &
+nohup ./bin/wildfires_cu data/input/debug.txt > wildfire.out 2>&1 &
