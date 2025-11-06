7894a6a981854a4ee92813694717f2f7dcfad155
Daniel San Martin <dsanmartinreyes@gmail.com>
2025-11-06 09:21:55 -0300

    Testing grid sizes

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,3 +3,4 @@
 # nohup ./bin/wildfires data/input/parameters.txt > wildfire.out 2>&1 &
 # nohup ./bin/wildfires_cu data/input/f19_gaussian.txt &
 nohup ./bin/wildfires_cu data/input/debug.txt > wildfire.out 2>&1 &
+# nohup ./bin/wildfires_cu data/input/complexity_analysis.txt > wildfire_complexity_analysis.out 2>&1 &
